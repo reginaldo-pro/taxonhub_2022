8d234328984853f724f1e022fb5c4817797fe69e
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-04-19 16:10:44 -0300

    fix: 🐛 Fixed missing routes / csv function call

--- src/routes/taxonomy.routes.ts
+++ src/routes/taxonomy.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 import { importCSVController } from "../modules/taxonomy/useCases/importCSV";
+import { listTaxonomiesController } from "../modules/taxonomy/useCases/listTaxonomies";
 
 const taxonomyRoutes = Router();
 const upload = multer({ 
@@ -14,7 +15,7 @@ const upload = multer({
     res.send("Hello taxonomy");
 });
 
-
+taxonomyRoutes.get("/list", (req,res) => { return listTaxonomiesController.handle(req,res)} )
 taxonomyRoutes.post("/import", upload.single("file"), (req,res) => {return importCSVController.handle(req,res)})
 
 
