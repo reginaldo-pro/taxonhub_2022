f235a32b90d63a6809b0f9c0f4f32594d63d32d8
Arthur Han <arthurhan269@gmail.com>
2022-04-20 01:05:18 -0300

    fix linting and options

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -3,9 +3,13 @@ import express from 'express';
 import { router } from './routes';
 
 const app = express();
+const port = 3333;
+
 app.use(express.json());
 app.use(router);
 
 app.get('/', (_, res) => res.send('Hello mundo'));
 
-app.listen('3333');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
