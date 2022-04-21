cb5aac15859d59430d631be274cafdab4e7705fe
Arthur Han <arthurhan269@gmail.com>
2022-04-21 03:43:30 -0300

    removed the save to db route from wfo module

--- back-end/src/routes/wfo.routes.ts
+++ back-end/src/routes/wfo.routes.ts
@@ -1,12 +1,7 @@
 import { Router } from 'express';
 import { getVersionController } from 'src/modules/wfo/useCases/getVersion';
-import { saveCSVtoDBController } from 'src/modules/wfo/useCases/saveCSVtoDB';
 
 const wfoRoutes = Router();
-
-wfoRoutes.post('/savetodb', (req, res) => {
-    return saveCSVtoDBController.handle(req, res);
-});
 
 wfoRoutes.get('/version', (req, res) => {
     return getVersionController.handle(req, res);
