20690b5a68dc29dedf6a79b243bee643448acd22
Arthur Han <arthurhan269@gmail.com>
2022-04-27 14:34:53 -0300

    Update IWfoRepository.ts

--- back-end/src/modules/wfo/repositories/IWfoRepository.ts
+++ back-end/src/modules/wfo/repositories/IWfoRepository.ts
@@ -1,7 +1,12 @@
+import { IRecord } from 'src/modules/model/WFORecord';
+
 interface IWfoRepository {
     saveTxtToDB(pathToFile: string): Promise<void>;
     getSavedVersion(): Promise<string>;
     updateVersion(version: string): Promise<void>;
+    getRecord(taxonID: string): Promise<IRecord>;
+    saveRecord(data: IRecord): Promise<void>;
+    updateRecord(data: IRecord): Promise<void>;
 }
 
 export { IWfoRepository };
