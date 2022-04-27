0ae6778ca23c303723c4483c2e79335bd15b69a8
Arthur Han <arthurhan269@gmail.com>
2022-04-27 14:35:27 -0300

    Update getVersionController.ts

--- back-end/src/modules/wfo/useCases/getVersion/getVersionController.ts
+++ back-end/src/modules/wfo/useCases/getVersion/getVersionController.ts
@@ -1,29 +1,20 @@
 import { Request, Response } from 'express';
 
 import { GetVersionUseCase } from './getVersionUseCase';
+import { IVersionData } from './types';
 
 class GetVersionController {
     constructor(private getVersionUseCase: GetVersionUseCase) {}
 
-    async handle(request: Request, response: Response) {
+    async handle(_request: Request, response: Response) {
         try {
-            const data = await this.getVersionUseCase.execute();
+            const data: IVersionData = await this.getVersionUseCase.execute();
 
             return response.status(200).json(data);
         } catch (error) {
             return response.status(500).json({ error: error.message });
         }
     }
-
-    // todo: Finish this task
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    updateVersion = async (request: Request, response: Response) => {
-        try {
-            throw new Error('Not implemented');
-        } catch (error) {
-            return response.status(500).json({ error: error.message });
-        }
-    };
 }
 
 export { GetVersionController };
