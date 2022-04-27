eeab6177e55558052f7749b19bfa64300faea698
Arthur Han <arthurhan269@gmail.com>
2022-04-27 14:36:32 -0300

    added new route to wfo router

--- back-end/src/routes/wfo.routes.ts
+++ back-end/src/routes/wfo.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { getVersionController } from 'src/modules/wfo/useCases/getVersion';
+import { updateDatabaseController } from 'src/modules/wfo/useCases/updateDatabase';
 
 const wfoRoutes = Router();
 
 wfoRoutes.get('/version', (req, res) => {
     return getVersionController.handle(req, res);
+});
+
+wfoRoutes.patch('/update', (req, res) => {
+    return updateDatabaseController.handle(req, res);
 });
 
 export { wfoRoutes };
