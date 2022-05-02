a62af49fefb6d4adef686dcdb789cbcc97e92077
Lucas Donadi <lucasdonadi@gmail.com>
2022-05-02 15:32:18 -0300

    Boot do projeto back-end

---
+++ back-end/src/routes.ts
@@ -0,0 +1,10 @@
+import  { response, Router } from "express"
+
+const router =  Router()
+
+router.post('/users', (request, response) => {
+    return response.status(201).send();
+})
+
+export { router } 
+ 
