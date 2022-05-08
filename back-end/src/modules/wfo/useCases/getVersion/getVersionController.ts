690c70e23026f4d6485e37f2f25e19dc54847fc9
Arthur Han <arthurhan269@gmail.com>
2022-05-07 21:18:54 -0300

    Fixed exception handling, response

--- back-end/src/modules/wfo/useCases/getVersion/getVersionController.ts
+++ back-end/src/modules/wfo/useCases/getVersion/getVersionController.ts
@@ -1,19 +1,16 @@
 import { Request, Response } from 'express';
+import { DefaultResponse } from 'src/modules/http/defaultResponse';
 
 import { GetVersionUseCase } from './getVersionUseCase';
-import { IVersionData } from './types';
 
 class GetVersionController {
     constructor(private getVersionUseCase: GetVersionUseCase) {}
 
     async handle(_request: Request, response: Response) {
-        try {
-            const data: IVersionData = await this.getVersionUseCase.execute();
+        const data: DefaultResponse<unknown> =
+            await this.getVersionUseCase.executeResponse();
 
-            return response.status(200).json(data);
-        } catch (error) {
-            return response.status(500).json({ error: error.message });
-        }
+        return response.status(data.status).json(data);
     }
 }
 
