c17a092c5b0e274525f164a243eb093177efedc9
Vinicius Kuwakino <vikuwakino@gmail.com>
2022-05-10 04:48:43 -0300

    parser-csv

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,4 +1,4 @@
-import  { response, Router } from "express"
+import  { Request, Response, Router } from "express"
 
 const router =  Router()
 
@@ -6,6 +6,7 @@ import { Readable } from "stream";
 import multer from 'multer';
 import csv from "fast-csv";
 import readline from "readline";
+import fs from "fs";
 
 const upload = multer();
 
@@ -13,25 +14,23 @@ router.post('/users', (request, response) => {
     return response.status(201).send();
 })
 
-router.post('/img', upload.single("filecsv"), async (request, response) => {
-    const buffer = request.file?.buffer;
-    // console.log(buffer)
-    const readableFile = new Readable();
-    readableFile.push(buffer);
+router.post('/img', upload.single("filecsv"), async (req: Request, res: Response) => {
+    const arrayNames = []
+    const b = req.file?.buffer
+    
+    const readableFile = new Readable()
+    readableFile.push(b)
+    readableFile.push(null)
 
-    // ####ERROR AO USAR READABLEFILE
+    const nameLine = readline.createInterface({
+        input: readableFile
+    })
 
-    console.log(readableFile)
+    for await(let line of nameLine) {
+        arrayNames.push(line)
+    }
 
-    // const speciesLine = readline.createInterface({
-    //     input: readableFile
-    // });
-
-    // for await (let line of speciesLine) {
-    //     console.log(line)
-    // }
-
-    return response.json("Testando");
+    return res.json(arrayNames);
 })
 
 export { router } 
