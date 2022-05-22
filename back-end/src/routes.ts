c81ae2c6af880d8d446d2b013efc97dd79c96e30
Victor Daniel <victor.pires@appmoove.com.br>
2022-05-22 17:58:35 -0300

    feat: created parse output flora do brasil
    Created parser output flora do brasil

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,6 +1,11 @@
 import  { response, Router } from "express"
+import { FloraDoBrasil } from "./Parser/FloraDoBrasil";
 
 const router =  Router()
+
+
+FloraDoBrasil.parseOutputFloraDoBrasil('Eichhornia azurea');
+
 
 router.post('/users', (request, response) => {
     return response.status(201).send();
