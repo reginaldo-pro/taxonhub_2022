8ea102974d85dab96bd34fcdf8c1c349c8a82ee9
Guilherme Vasco <guilhermevasco08@gmail.com>
2022-05-22 14:55:47 -0300

    Revert "Api flora#31"

--- src/Backend/api/routes/home.js
+++
@@ -1,6 +0,0 @@
-module.exports = app => {
-  const controller = app.controllers.home;
-
-  app.route('/')
-    .get(controller.startApi);
-}
--- src/Backend/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./config/express');
-const port = app.get('port');
-
-app.listen(port, ()=> {
-    console.log(`Aplicação rodando em http://localhost:${port}`);
-});
