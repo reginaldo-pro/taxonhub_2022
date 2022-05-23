8afa398cbdde8a6d07676a3b70e0259d8b661127
Victor Daniel <victor.pires@appmoove.com.br>
2022-05-22 22:04:16 -0300

    #13 - removed unused route

--- back-end/src/routes.ts
+++ back-end/src/routes.ts
@@ -1,17 +1,9 @@
 import  { response, Router } from "express"
-import { FloraDoBrasil } from "./Parser/FloraDoBrasil";
 import multer from 'multer'
 import { Controller } from './Controllers/Controller'
 
 const router = Router()
 
-
-FloraDoBrasil.parseOutputFloraDoBrasil('Eichhornia azurea');
-
-
-router.post('/users', (request, response) => {
-    return response.status(201).send();
-})
 const upload = multer()
 
 const controller = new Controller()
