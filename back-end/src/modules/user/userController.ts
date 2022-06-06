14d179e887cb90fab8f48c8149b5e62d7f22c730
Arthur Han <arthurhan269@gmail.com>
2022-06-06 20:35:54 -0300

    refactor: added typing and return type to user service

--- back-end/src/modules/user/userController.ts
+++ back-end/src/modules/user/userController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
 import { DefaultResponse } from 'src/modules/http/defaultResponse';
 
-import { userUtils } from './userUtils';
+import { IUserDetails, userUtils } from './userUtils';
 
 class UserController {
-    handle(_request: Request, response: Response) {
-        const data: DefaultResponse<unknown> = userUtils.generateId();
+    handle(_request: Request, response: Response): Response {
+        const data: DefaultResponse<IUserDetails> = userUtils.generateId();
 
         return response.status(data.status).json(data);
     }
