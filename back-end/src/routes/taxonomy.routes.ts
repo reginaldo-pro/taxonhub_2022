08717ca03dbeca5dc6ccace35870433895d363d9
Arthur Han <arthurhan269@gmail.com>
2022-06-14 21:10:24 -0300

    refactor: removed unused import route

--- back-end/src/routes/taxonomy.routes.ts
+++ back-end/src/routes/taxonomy.routes.ts
@@ -1,29 +1,10 @@
 import { Router } from 'express';
-import multer from 'multer';
-import { FILES_FOLDER } from 'src/modules/config/constants';
 import { generateCSVController } from 'src/modules/taxonomy/useCases/generateCSV';
-import { importBinomialNameCSVController } from 'src/modules/taxonomy/useCases/importBinomialNameCSV';
 
 const taxonomyRoutes = Router();
 
-const storage = multer.diskStorage({
-    destination: FILES_FOLDER,
-    filename(req, file, cb) {
-        const { userId } = req.body;
-        cb(null, `${userId}-binomialNames.csv`);
-    },
-});
-
-const upload = multer({
-    storage,
-});
-
 taxonomyRoutes.get('/generatecsv', (req, res) => {
     return generateCSVController.handle(req, res);
-});
-
-taxonomyRoutes.post('/import', upload.single('file'), (req, res) => {
-    return importBinomialNameCSVController.handle(req, res);
 });
 
 export { taxonomyRoutes };
