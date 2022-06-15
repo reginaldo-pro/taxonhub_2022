bd82fa49d9b34ff32ab6cc4041d7397b94c8451e
Arthur Han <arthurhan269@gmail.com>
2022-06-14 21:09:59 -0300

    perf: removed dead import and code

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -5,7 +5,6 @@ import { importBinomialNameCSVController } from 'src/modules/taxonomy/useCases/i
 
 import { ocurrencyRoutes } from './occurrency.routes';
 import { taxonomyRoutes } from './taxonomy.routes';
-import { userRoutes } from './user.routes';
 import { wfoRoutes } from './wfo.routes';
 
 const storage = multer.diskStorage({
@@ -25,7 +24,6 @@ const router = Router();
 router.use('/taxonomy', taxonomyRoutes);
 router.use('/occurrency', ocurrencyRoutes);
 router.use('/wfo', wfoRoutes);
-router.use('/user', userRoutes);
 
 router.post('/import', upload.single('file'), (req, res) => {
     return importBinomialNameCSVController.handle(req, res);
