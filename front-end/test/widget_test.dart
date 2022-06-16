83efb45b4cedce591ec7368700ba09dd0416fc58
Victor Daniel <victor.pires@appmoove.com.br>
2022-06-15 23:53:09 -0300

    fix: test.dart
    fized error in test.dart

--- front-end/test/widget_test.dart
+++ front-end/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:front_end/main.dart';
+import 'package:front_end/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
