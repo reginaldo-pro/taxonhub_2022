d2bacdaa238ba7d28d7df7c6254b00ac6c64dfa6
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2022-06-16 02:05:44 -0300

    feat: 🎸 download route + occurrency screen

--- back-end/src/routes/occurrency.routes.ts
+++ back-end/src/routes/occurrency.routes.ts
@@ -1,10 +1,18 @@
 import { Router } from 'express';
 import { getOccurrencyController } from 'src/modules/occurrency/useCases/getOccurrency';
-
+import { FILES_FOLDER } from 'src/modules/config/constants';
 const ocurrencyRoutes = Router();
 
 ocurrencyRoutes.get('/generatecsv', (req, res) => {
     return getOccurrencyController.handle(req, res);
 });
+
+ocurrencyRoutes.get('/download', (req, res) => {
+    console.log('downloading taxonomies');
+    const { userId } = req.query;
+    res.download(`${FILES_FOLDER}/${userId}-occurrency.csv`);
+}
+);
+
 
 export { ocurrencyRoutes };
