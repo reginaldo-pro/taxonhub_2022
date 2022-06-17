e776e1a7b150d6f7ccf20c2ec9cb82ca0b69d400
Victor Daniel <victor.pires@appmoove.com.br>
2022-06-17 00:00:59 -0300

    fix: change name icon
    changed icon name from donwload to download. Added new translations and
    new colors.

--- front-end/lib/colors/colors.dart
+++ front-end/lib/colors/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorsApp {
   static const Color backgroundColor = Color.fromRGBO(193, 222, 174, 0.5);
+  static const Color backgroundAppBar = Color.fromRGBO(255, 255, 255, 0.0);
   static const Color basilSmash = Color.fromRGBO(193, 222, 174, 1.0);
   static const Color tropicalForest = Color.fromRGBO(0, 121, 85, 1.0);
   static const Color blursModalBackground = Color.fromRGBO(54, 58, 69, 0.75);
--- front-end/lib/icons/icons.dart
+++ front-end/lib/icons/icons.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class IconsApp {
   static const IconData uploud = Icons.file_upload_outlined;
-  static const IconData donwload = Icons.file_download_outlined;
+  static const IconData download = Icons.file_download_outlined;
   static const IconData warning = Icons.warning_amber_outlined;
   static const IconData error = Icons.disabled_by_default_outlined;
   static const IconData success = Icons.check_circle_outlined;
